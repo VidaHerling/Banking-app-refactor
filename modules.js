a927b405d65951459eadcc9084f531621d4ec18c
Vida <herling.codingcamp@gmail.com>
2022-08-29 21:14:38 -0400

    add all the modules and onClick functions

--- modules.js
+++ modules.js
@@ -9,25 +9,25 @@ ui.navigation = `<nav class="navbar navbar-expand-lg bg-light">
   <div class="collapse navbar-collapse" id="navbarSupportedContent">
     <ul class="navbar-nav me-auto mb-2 mb-lg-0">
       <li class="nav-item">
-        <a class="nav-link active" aria-current="page" href="#">Home</a>
+        <a class="nav-link active" aria-current="page" href="#" onClick="loadHome()">Home</a>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="#CreateAccount">Create Account</a>
+        <a class="nav-link" href="#CreateAccount" onClick="loadCreateAccount()">Create Account</a>
       </li>
       <li class="nav-item">
-      <a class="nav-link" href="#Login">Login</a>
+      <a class="nav-link" href="#Login" onClick="loadLogin()">Login</a>
       </li>
       <li class="nav-item">
-      <a class="nav-link" href="#Deposit">Deposit</a>
+      <a class="nav-link" href="#Deposit" onClick="loadDeposit()">Deposit</a>
       </li>
       <li class="nav-item">
-      <a class="nav-link" href="#Withdraw">Withdraw</a>
+      <a class="nav-link" href="#Withdraw" onClick="loadWithdraw()">Withdraw</a>
       </li>
       <li class="nav-item">
-      <a class="nav-link" href="#Balance">Balance</a>
+      <a class="nav-link" href="#Balance" onClick="loadBalance()">Balance</a>
       </li>
       <li class="nav-item">
-      <a class="nav-link" href="#AllData">All Data</a>
+      <a class="nav-link" href="#AllData" onClick="loadAllData()">All Data</a>
       </li>
     </ul>
   </div>
@@ -37,3 +37,113 @@ ui.navigation = `<nav class="navbar navbar-expand-lg bg-light">
 
 const navigation = document.getElementById("navigation");
 navigation.innerHTML += ui.navigation;
+const page = document.getElementById("root");
+
+ui.home = `
+<div class="card" style="width: 18rem;">
+  <img src="..." class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Steadfast Bank Homepage</h5>
+    <p class="card-text">Welcome to the bank!</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
+`;
+const loadHome = () => {
+  page.innerHTML = ui.home;
+}
+
+ui.createAccount = `
+<div class="card" style="width: 18rem;">
+  <img src="bank_account.jpeg" class="card-img-top" alt="bank account">
+  <div class="card-body">
+    <h5 class="card-title">Create Account</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary">Create Account</a>
+  </div>
+</div>
+`;
+
+const loadCreateAccount = () => {
+  page.innerHTML = ui.createAccount;
+}
+
+ui.login = `
+<div class="card" style="width: 18rem;">
+  <img src="bank_account.jpeg" class="card-img-top" alt="bank account">
+  <div class="card-body">
+    <h5 class="card-title">Log in</h5>
+    <input id="email" type="email" placeholder="Email">
+    <input id="password" type="password" placeholder="Password">
+    <a href="#" class="btn btn-primary">Login</a>
+  </div>
+</div>
+`;
+
+const loadLogin = () => {
+  page.innerHTML = ui.login;
+}
+
+ui.deposit = `
+<div class="card" style="width: 18rem;">
+  <img src="bank_account.jpeg" class="card-img-top" alt="bank account">
+  <div class="card-body">
+    <h5 class="card-title">Log in</h5>
+    <input id="email" type="email" placeholder="Email">
+    <input id="password" type="password" placeholder="Password">
+    <a href="#" class="btn btn-primary">Login</a>
+  </div>
+</div>
+`;
+
+const loadDeposit = () => {
+  page.innerHTML = ui.deposit;
+}
+
+ui.withdraw = `
+<div class="card" style="width: 18rem;">
+  <img src="bank_account.jpeg" class="card-img-top" alt="bank account">
+  <div class="card-body">
+    <h5 class="card-title">Log in</h5>
+    <input id="email" type="email" placeholder="Email">
+    <input id="password" type="password" placeholder="Password">
+    <a href="#" class="btn btn-primary">Login</a>
+  </div>
+</div>
+`;
+
+const loadWithdraw = () => {
+  page.innerHTML = ui.withdraw;
+}
+
+ui.balance = `
+<div class="card" style="width: 18rem;">
+  <img src="bank_account.jpeg" class="card-img-top" alt="bank account">
+  <div class="card-body">
+    <h5 class="card-title">Log in</h5>
+    <input id="email" type="email" placeholder="Email">
+    <input id="password" type="password" placeholder="Password">
+    <a href="#" class="btn btn-primary">Login</a>
+  </div>
+</div>
+`;
+
+const loadBalance = () => {
+  page.innerHTML = ui.balance;
+}
+
+ui.allData = `
+<div class="card" style="width: 18rem;">
+  <img src="bank_account.jpeg" class="card-img-top" alt="bank account">
+  <div class="card-body">
+    <h5 class="card-title">Log in</h5>
+    <input id="email" type="email" placeholder="Email">
+    <input id="password" type="password" placeholder="Password">
+    <a href="#" class="btn btn-primary">Login</a>
+  </div>
+</div>
+`;
+
+const loadAllData = () => {
+  page.innerHTML = ui.allData;
+}
