cbf69af798860db7c0a73ba7ef939786da3ab167
Vida <herling.codingcamp@gmail.com>
2022-08-31 23:09:40 -0400

    refactor - fetch data from db

--- public/alldata.js
+++ public/alldata.js
@@ -1,9 +1,20 @@
 function AllData(){
-  const ctx = React.useContext(UserContext);
+  const [data, setData] = React.useState('');
+  
+  React.useEffect(() => {
+    // fetch all data from API
+    fetch('/account/all')
+      .then(res => res.json())
+      .then(data => {
+        console.log(data);
+        setData(JSON.stringify(data));
+      });
+  }, []);
+
   return (
     <div>
-      <h1>All Data Component</h1>
-      {JSON.stringify(ctx)}
+      <h1>All Data shown here:</h1>
+      {data}
     </div>
-  )
+  );
 }
